c26e91ee2ed41e26c1f91a05e44123c7de684bd9
Dennis McDaid <dmcdaid95@gmail.com>
2019-12-02 00:49:09 +1100

    Clean up Day 01 code.

--- src/main/kotlin/com/dennismcdaid/advent2019/Day01.kt
+++ src/main/kotlin/com/dennismcdaid/advent2019/Day01.kt
@@ -1,7 +1,6 @@
 package com.dennismcdaid.advent2019
 
 import org.jetbrains.annotations.TestOnly
-import kotlin.math.floor
 
 object Day01 {
 
@@ -9,33 +8,29 @@ object Day01 {
    * Fuel required to launch a given module is based on its mass.
    * Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
    */
-  fun mapMassToFuel(mass: Int) : Int {
-    return floor(mass.toDouble() / 3).toInt() - 2
-  }
+  fun mapMassToFuel(mass: Int): Int =
+    mass / 3 - 2
 
   /**
    * So, for each module mass, calculate its fuel and add it to the total.
    * Then, treat the fuel amount you just calculated as the input mass and repeat the process,
    * continuing until a fuel requirement is zero or negative.
    */
-  private fun reduceFuel(totalFuel: Int, currentFuelMass: Int = totalFuel) : Int {
+  private fun reduceFuel(totalFuel: Int, currentFuelMass: Int = totalFuel): Int {
     val newFuel = mapMassToFuel(currentFuelMass)
     return if (newFuel <= 0) totalFuel
     else reduceFuel(totalFuel + newFuel, newFuel)
   }
 
-  fun reduceRawMassToFuel(masses: List<Int>) : Int {
-    return masses.map(this::mapMassToFuel).sum()
-  }
+  fun reduceRawMassToFuel(masses: List<Int>): Int =
+    masses.sumBy(this::mapMassToFuel)
 
-  fun reduceTotalMassToFuel(masses: List<Int>) : Int {
-    return masses.map(this::mapMassToFuel)
-      .map { reduceFuel(it) }
-      .sum()
-  }
+  fun reduceTotalMassToFuel(masses: List<Int>): Int =
+    masses.map(this::mapMassToFuel)
+      .sumBy { reduceFuel(it) }
 
   @TestOnly
-  fun reduceIndividualMassToFuel(mass: Int) : Int {
+  fun reduceIndividualMassToFuel(mass: Int): Int {
     val baseFuel = mapMassToFuel(mass)
     return reduceFuel(baseFuel)
   }
