43cb4ed76d0dc23f1dd6a8e9181b0168443d7600
Dennis McDaid <dmcdaid95@gmail.com>
2019-12-06 12:47:32 +1100

    Finally crack part 1.

--- src/main/kotlin/com/dennismcdaid/advent2019/Day05.kt
+++ src/main/kotlin/com/dennismcdaid/advent2019/Day05.kt
@@ -2,66 +2,87 @@ package com.dennismcdaid.advent2019
 
 object Day05 {
 
-  sealed class InstructionResult {
-    object Finished : InstructionResult()
-    data class Success(val incrementBy: Int) : InstructionResult()
+  sealed class Instruction(val size: Int) {
+    abstract val modes: List<Int>
+    abstract fun execute(intArray: IntArray, index: Int)
+
+    protected fun getValueForMode(intArray: IntArray, index: Int, mode: Int) : Int {
+      return if (mode == 0) intArray[intArray[index]] else intArray[index]
+    }
+
+    data class Addition(override val modes: List<Int>) : Instruction(4) {
+      override fun execute(intArray: IntArray, index: Int) {
+        val a = getValueForMode(intArray, index + 1, modes[0])
+        val b = getValueForMode(intArray, index + 2, modes[1])
+
+        intArray[intArray[index + 3]] = a + b
+      }
+    }
+
+    data class Multiplication(override val modes: List<Int>) : Instruction(4) {
+      override fun execute(intArray: IntArray, index: Int) {
+        val a = getValueForMode(intArray, index + 1, modes[0])
+        val b = getValueForMode(intArray, index + 2, modes[1])
+
+        intArray[intArray[index + 3]] = a * b
+      }
+    }
+
+    data class Save(val input: Int) : Instruction(2) {
+      override val modes: List<Int> = emptyList()
+      override fun execute(intArray: IntArray, index: Int) {
+        intArray[intArray[index + 1]] = input
+      }
+    }
+
+    data class Output(override val modes: List<Int>) : Instruction(2) {
+      override fun execute(intArray: IntArray, index: Int) {
+        // Print when it's not 0
+        getOutput(intArray, index).takeIf { it != 0 }?.let(::println)
+      }
+
+      fun getOutput(intArray: IntArray, index: Int) : Int {
+        return getValueForMode(intArray, index + 1, modes[0])
+      }
+    }
+
+    object Complete : Instruction(1) {
+      override val modes: List<Int> = emptyList()
+      override fun execute(intArray: IntArray, index: Int) {}
+    }
   }
 
-  data class Instruction(val opcode: Int, val modes: List<Int>) {
+  fun generateInstruction(command: Int, input: Int = 0) : Instruction {
+    val opcode = command % 100
+    val modes = (command / 100)
+      .toString()
+      .let { "000$it" }
+      .toCharArray()
+      .map(Character::getNumericValue)
+      .reversed()
+      .subList(0, 3)
 
-    fun perform(existing: IntArray, parentIndex: Int) : InstructionResult {
-      val params = modes.mapIndexed { index, i ->
-        val param = existing[parentIndex+index]
-        return@mapIndexed when (i) {
-          // Position mode
-          0 -> existing[param]
-          1 -> param
-          else -> throw IllegalArgumentException("Unsupported mode: $i")
-        }
-      }
-      when (opcode) {
-        // Addition
-        1 -> existing[params[2]] = existing[params[0]] + existing[params[1]]
-        // Multiplication
-        2 -> existing[params[2]] = existing[params[0]] * existing[params[1]]
-        // Write
-        // This is custom
-        3 -> existing[params[0]] = 1
-        // Output
-        4 -> println("State: ${existing[params[0]]}")
-        // Complete
-        99 -> return InstructionResult.Finished
-      }
-
-      return InstructionResult.Success(params.size + 1)
+    return when (opcode) {
+      1 -> Instruction.Addition(modes)
+      2 -> Instruction.Multiplication(modes)
+      3 -> Instruction.Save(input)
+      4 -> Instruction.Output(modes)
+      99 -> Instruction.Complete
+      else -> throw IllegalArgumentException("No value for opcode $opcode")
     }
+  }
 
-    companion object {
-      fun from(code: Int) : Instruction {
-        val opcode = code % 100
-        val rawModes = (code / 100).toString().map(Character::getNumericValue).reversed()
-
-        val modes = if (rawModes.size == sizeForOpcode(opcode)) rawModes
-        else rawModes + (rawModes.size until sizeForOpcode(opcode)).map { 0 }
-
-        return Instruction(opcode, modes)
-      }
-
-      private fun sizeForOpcode(opcode: Int) : Int = when (opcode) {
-        // Addition
-        1 -> 3
-        // Multiplication
-        2 -> 3
-        // Write
-        // This is custom
-        3 -> 1
-        // Output
-        4 -> 1
-        // Complete
-        99 -> 0
-        else -> throw IllegalArgumentException("Unsupported opcode: $opcode")
-      }
-    }
+  fun execute(intArray: IntArray, input: Int) : Int {
+    val outputs = mutableListOf<Int>()
+    var instruction: Instruction
+    var index = 0
+    do {
+      instruction = generateInstruction(intArray[index], input)
+      instruction.execute(intArray, index)
+      (instruction as? Instruction.Output)?.getOutput(intArray, index)?.let(outputs::add)
+      index += instruction.size
+    } while (instruction !is Instruction.Complete)
+    return outputs.last()
   }
 
 }
--- src/test/kotlin/com/dennismcdaid/advent2019/Day05Test.kt
+++ src/test/kotlin/com/dennismcdaid/advent2019/Day05Test.kt
@@ -17,7 +17,30 @@ class Day05Test {
 
     @Test
     fun `Instruction generates correctly`() {
-      assertThat(Day05.Instruction.from(1002)).isEqualTo(Day05.Instruction(2, listOf(0, 1, 0)))
+      assertThat(Day05.generateInstruction(3)).isInstanceOf(Day05.Instruction.Save::class.java)
+      assertThat(Day05.generateInstruction(4)).isInstanceOf(Day05.Instruction.Output::class.java)
+      assertThat(Day05.generateInstruction(1)).isInstanceOf(Day05.Instruction.Addition::class.java)
+      assertThat(Day05.generateInstruction(1002)).isInstanceOf(Day05.Instruction.Multiplication::class.java)
+      assertThat(Day05.generateInstruction(99)).isInstanceOf(Day05.Instruction.Complete::class.java)
+      assertThat(Day05.generateInstruction(1002).modes).isEqualTo(listOf(0, 1, 0))
+      assertThat(Day05.generateInstruction(11101).modes).isEqualTo(listOf(1, 1, 1))
+    }
+
+    @Test
+    fun `Operation performs correctly`() {
+      val example = intArrayOf(1002, 4, 3, 4, 33)
+      val expectedOutput = intArrayOf(1002, 4, 3, 99, 33)
+      val index = 0
+      val instruction = Day05.generateInstruction(example[index])
+      assertThat(instruction).isInstanceOf(Day05.Instruction.Multiplication::class.java)
+      assertThat(instruction.modes).isEqualTo(listOf(0, 1, 0))
+      instruction.execute(example, index)
+      assertThat(example).isEqualTo(expectedOutput)
+    }
+
+    @Test
+    fun `Part 1 executes`() {
+      assertThat(Day05.execute(input, 1)).isEqualTo(12896948)
     }
   }
 }
